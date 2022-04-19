b4cf0de71d30d040067e7cd99b5ee38c1f926bdc
leonitshaqiri <65737961+leonitshaqiri@users.noreply.github.com>
2022-04-19 18:53:53 +0200

    336. Data Fetching for Static Pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -20,15 +19,17 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
 
-  useEffect(() => {
-    // send a http and fetch data
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
-
-  return <MeetupList meetups={loadedMeetups} />;
+export async function getStaticProps() {
+  // fetch data from API.
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
